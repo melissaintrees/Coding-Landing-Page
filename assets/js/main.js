cd5c385ed0949faa1384f8704976baee5c0048e2
Melissa St Moore <melissaintrees@gmail.com>
2018-06-15 19:33:10 -0500

    clean up files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,39 +4,23 @@
 
   $(window).on('load', function() {
 
-  /* Changing the chevron from open to close */
-    $('#accordion').on('shown.bs.collapse', function (e) {
-      $(e.target)
-        .data("bs.collapse")
-        .$trigger.attr("src", "https://via.placeholder.com/350x150");
-    });
-
-    $('#accordion').on('hidden.bs.collapse', function (e, f) {
-      console.log($(e.target).data('bs.collapse').$trigger);
-      //$(this).find(".panel-icon").attr('src', 'https://s22.postimg.org/9takvqkal/Mathematic_Plus2.png');
-      $(e.target)
-        .data("bs.collapse")
-        .$trigger.attr("src", "../img/about.jpg");
-    });
-
-
 
   /*Page Loader active */
   $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 50) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
+  // // Sticky Nav
+  //   $(window).on('scroll', function() {
+  //       if ($(window).scrollTop() > 50) {
+  //           $('.scrolling-navbar').addClass('top-nav-collapse');
+  //       } else {
+  //           $('.scrolling-navbar').removeClass('top-nav-collapse');
+  //       }
+  //   });
 
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-      currentClass: 'active'
-    });
+  //   // one page navigation 
+  //   $('.navbar-nav').onePageNav({
+  //     currentClass: 'active'
+  //   });
 
 
       /* WOW Scroll Spy
@@ -130,12 +114,7 @@
         return false;
       });
 
-      /* Map Form Toggle
-      ========================================================*/
-      $('.map-icon').on('click',function (e) {
-          $('#conatiner-map').toggleClass('panel-show');
-          e.preventDefault();
-      });
+
 
   });      
 
